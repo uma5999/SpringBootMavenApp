0ffa4a8248af5c266861656b017a324afdc1f1af
AMAIISDOM\ugolakaram <umamaheshwarrao.golakaram@amadeus.com>
2022-07-03 23:57:50 +0530

    entire project to repo

---
+++ src/main/java/com/techi/sptringproject/SptringprojectApplication.java
@@ -0,0 +1,13 @@
+package com.techi.sptringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SptringprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SptringprojectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/techi/sptringproject/controllers/MyController.java
@@ -0,0 +1,19 @@
+package com.techi.sptringproject.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tech")
+public class MyController {
+
+	@GetMapping("/string/1")
+	public String m1() {
+		return "Hello1";
+	}
+	@GetMapping("/string/2")
+	public String m2() {
+		return "Hello2";
+	}
+}
